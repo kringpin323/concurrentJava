73c46bed59c1d03a52b0084402ca11634a93c290
kringpin323 <kringpin323@gmail.com>
2014-09-10 15:28:01 +0800

    暂时停下，从《Java并发》跳去《Java线程》

--- src/net/jcip/examples/SafeListener.java
+++ src/net/jcip/examples/SafeListener.java
@@ -4,7 +4,8 @@ package net.jcip.examples;
  * SafeListener
  * <p/>
  * Using a factory method to prevent the this reference from escaping during construction
- *
+ * 使用工厂方法防止引用逸出
+ * @since 2014-09-10
  * @author Brian Goetz and Tim Peierls
  */
 public class SafeListener {
--- src/net/jcip/examples/ThisEscape.java
+++ src/net/jcip/examples/ThisEscape.java
@@ -4,7 +4,10 @@ package net.jcip.examples;
  * ThisEscape
  * <p/>
  * Implicitly allowing the this reference to escape
- *
+ * 隐式地允许this引用逸出（不明白？）
+ * 个人理解：在ThisEscape构造函数完全完成 EventListener才算完成，
+ * 但是这里可能出现情况，EventListener有引用可用但未完全完成的时候就被其他线程使用
+ * Date：2014-09-10
  * @author Brian Goetz and Tim Peierls
  */
 public class ThisEscape {
